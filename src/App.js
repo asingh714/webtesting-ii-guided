92536abf249c085d3d4a03737c4e8931507ade27
Aman <amsingh714@gmail.com>
2019-04-09 13:35:42 -0400

    testing greet and bye

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    greeting: "No greeting for you",
+  }
+
+  greet = () => {
+    this.setState({
+      greeting: "hello web XVII"
+    })
+  }
+
+  bye = () => {
+    this.setState({
+      greeting: "Bye web XVII"
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <h1>Hello World</h1>
+        <button onClick={this.greet}>Greet</button>
+        <button onClick={this.bye}>Bye</button>
+
+        <div>{this.state.greeting}</div>
+      </>
     );
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { render } from "react-testing-library";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
 
+describe("<App />", () => {
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it("renders successfully", () => {
+    render(<App/>)
+  })
 
-it("renders successfully", () => {
-  render(<App/>)
+  it("renders hello world", () => {
+    const { getByText,queryByText, debug } = render(<App />)
+
+    const text = getByText(/hello world/i)
+    const text2 = queryByText(/hello world/i)
+
+    debug();
+
+    expect(text).toBeInTheDocument();
+    expect(text2).toBeTruthy(); // or
+    expect(text2).not.toBeNull();
+
+    getByText(/hello world/i)
+    // querybytext will return null instead of failing the test
+  }) 
+
+  it("greets the team", () => {
+    const { getByText } = render(<App />)
+
+    const button = getByText(/greet/i);
+
+    fireEvent.click(button);
+    getByText(/hello web XVII/i)
+  })
+
+  it("says goodbye", () => {
+    const { getByText } = render(<App />)
+
+    const byebutton = getByText(/bye/i);
+
+    fireEvent.click(byebutton);
+    getByText(/bye web XVII/i)
+  })
 })
